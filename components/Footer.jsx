da05d6da3cdaa898b4331d1f9c0b8098fcc83d33
romanromannya <619cogitoergosum@gmail.com>
2023-05-08 14:48:11 +0700

    quran everywhere

---
+++ components/Footer.jsx
@@ -0,0 +1,17 @@
+export default function Footer() {
+  return (
+    <>
+      <footer className="p-3 text-center mb-20">
+        <p>
+          Dibuat dengan ❤ oleh{' '}
+          <a href="https://lrmn.is-a.fun" target="_blank">
+            L RMN
+          </a>{' '}
+          {/* untuk Sweet heartnya */}
+        </p>
+      </footer>
+    </>
+  )
+}
+
+// 16/02/2022 16.00 WIB
---
+++ constants/location.js
@@ -0,0 +1,6 @@
+module.exports = {
+  coords: {
+    lat: -6.21462,
+    lng: 106.84513,
+  },
+};
